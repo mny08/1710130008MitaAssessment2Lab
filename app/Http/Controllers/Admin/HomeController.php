31af36ef658f96e9fcaf051cb7590a5a66a7d6f9
Mita Nurul <m.nurul.y@students.esqbs.ac.id>
2020-04-19 23:34:31 +0700

    penambahan home controller

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -11,10 +11,10 @@ use App\Client;
 class HomeController
 {
     public function index()
-{
-    $employee = Employee::count();
-    $client = Client::count();
-    
-    return view('home', compact('employee', 'client'));
-}
+	{
+		$employee = Employee::count();
+		$client = Client::count();
+		
+		return view('home', compact('employee', 'client'));
+	}
 }
